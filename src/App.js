83a20404548ed868a141b258989ac8f47a3ae827
pashaumar <pasha.u.144@gmail.com>
2020-10-31 12:35:51 +0530

    app working fine as expected

--- src/App.js
+++ src/App.js
@@ -53,23 +53,35 @@ class App extends Component {
     })
     
   }
+  newGame = () =>{
+    this.setState(prevState =>{
+      return{
+        activePlayer: 'playerOne',
+        playerOneScore: 0,
+        playerTwoScore: 0,
+        currentOneScore: 0,
+        currentTwoScore: 0,
+        diceRandom: 1
+      }
+    })
+  }
 
   render() {
     return (
       <div className="container">
         {/* player -1 */}
-        <div className="player-1-container">
-          <div className="new-game">
+        <div className={`player-1-container ${this.state.activePlayer === "playerOne" && "active-player"}`}>
+          <div className="new-game" onClick={this.newGame}>
           <i className="fas fa-plus-circle"></i>
           <h1>New Game</h1>
           </div>
-          <div className="roll-dice" onClick={this.rollDice}>
+          <div className={`roll-dice ${this.state.playerOneScore >= 100 || this.state.playerTwoScore >= 100 ? "game-ended" : ""}`} onClick={this.rollDice}>
             <i className="fas fa-circle-notch"></i>
             <h1>Roll Dice</h1>
           </div>
           <div className="player-1-heading">
-            <h1 className="player-1-name">Player1</h1>
-            <div className="circle-1"></div>
+            <h1 className="player-1-name">{this.state.playerOneScore >= 100 ? "Winner" : "Player1"}</h1>
+            { this.state.activePlayer === 'playerOne' && <div className="circle-1"></div>}
           </div>
           <h1 className="player-1-score">{this.state.playerOneScore}</h1>
           <div className="current-player-1">
@@ -81,10 +93,10 @@ class App extends Component {
           </div>
         </div>
         {/* player 2 */}
-        <div className="player-2-container">
+        <div className={`player-2-container ${this.state.activePlayer === "playerTwo" && "active-player"}`}>
           <div className="player-2-heading">
-            <h1 className="player-2-name">Player1</h1>
-            <div className="circle-2"></div>
+          <h1 className="player-2-name">{this.state.playerTwoScore >= 100 ? "Winner" : "Player2"}</h1>
+            {this.state.activePlayer === 'playerTwo' && <div className="circle-2"></div>}
           </div>
           <h1 className="player-2-score">{this.state.playerTwoScore}</h1>
           <div className="current-player-2">
